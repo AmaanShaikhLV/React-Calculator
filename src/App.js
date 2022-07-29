d079ab6c63f17c3a61bce68e6ef589453f16559f
Amaan Shaikh <amaan.shaikh@launchventures.co>
2022-07-29 12:06:21 +0530

    Calculator In React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React,{useState} from "react";
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+  const clear = () => {
+    setResult("");
+  };
+  const calculate = () => {
+    try
+    {
+    setResult(eval(result).toString());
+    }
+    catch{
+      console.log("Error");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="output">
+        <form>
+          <input type="text" value={result} />
+        </form>
+      </div>
+      <button className="clear" onClick={clear} id="clear">clear</button>
+      <button className="operatand" onClick={handleClick} name="/">%</button>
+
+      <button onClick={handleClick} name="7">7</button>
+      <button onClick={handleClick} name="8">8</button>
+      <button onClick={handleClick} name="9">9</button>
+      <button className="operatand" onClick={handleClick} name="-">-</button>
+
+      <button onClick={handleClick} name="4">4</button>
+      <button onClick={handleClick} name="5">5</button>
+      <button onClick={handleClick} name="6">6</button>
+      <button className="operatand" onClick={handleClick} name="+">+</button>
+
+      <button onClick={handleClick} name="1">1</button>
+      <button onClick={handleClick} name="2">2</button>
+      <button onClick={handleClick} name="3">3</button>
+      <button className="operatand" onClick={calculate} id="result">=</button>
     </div>
   );
 }
